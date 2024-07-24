142cb4c3ca3138abe352471973b45cb70e3f5bb9
Phurin Nararat <phurin.nrt@gmail.com>
2024-07-24 19:39:55 +0700

    Add fav icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+      </head>
       <body className={inter.className + " bg-background text-highlight"}>{children}</body>
     </html>
   );
